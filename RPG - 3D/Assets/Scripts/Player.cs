9ef80c5f267ee9a9c3080466951ce0429ad6ff4d
xSuui <samuel2012rogerio@gmail.com>
2024-06-24 19:11:30 -0300

    adjusts

--- RPG - 3D/Assets/Scripts/Player.cs
+++ RPG - 3D/Assets/Scripts/Player.cs
@@ -15,16 +15,20 @@ public class Player : MonoBehaviour
     CharacterController controller;
     Animator anim;
 
+    bool IsReady;
+
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        GetMouseInput();
     }
 
     void Move()
@@ -33,12 +37,27 @@ public class Player : MonoBehaviour
         {
             if(Input.GetKey(KeyCode.W))
             {
-                MoveDirection = Vector3.forward * Speed;
-                MoveDirection = transform.TransformDirection(MoveDirection);
+                if(!anim.GetBool("attacking"))
+                { 
+                    anim.SetBool("walking", true);
+                    anim.SetInteger("transition", 1);
+                    MoveDirection = Vector3.forward * Speed;
+                    MoveDirection = transform.TransformDirection(MoveDirection);
+                }
+
+                else
+                {
+                    anim.SetBool("walking", false);
+                    anim.SetInteger("transition", 0);
+                    MoveDirection = Vector3.zero;
+                    StartCoroutine(Attack(1));
+                }
             }
 
             if (Input.GetKeyUp(KeyCode.W))
             {
+                anim.SetBool("walking", false);
+                anim.SetInteger("transition", 0);
                 MoveDirection = Vector3.zero;
             }
         }
@@ -48,5 +67,39 @@ public class Player : MonoBehaviour
 
         MoveDirection.y -= Gravity * Time.deltaTime;
         controller.Move(MoveDirection * Time.deltaTime);
+    }
+
+    void GetMouseInput()
+    {
+        if(controller.isGrounded)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(anim.GetBool("walking"))
+                {
+                    anim.SetBool("walking", false);
+                    anim.SetInteger("transition", 0);
+                }
+
+                if(!anim.GetBool("walking"))
+                {
+                    StartCoroutine(Attack(0));
+                }
+            }
+        }
+    }
+
+    IEnumerator Attack(int transitionValue)
+    {
+        if(!IsReady)
+        {
+            IsReady = true;
+            anim.SetBool("attacking", true);
+            anim.SetInteger("transition", 2);
+            yield return new WaitForSeconds(1.3f);
+            anim.SetInteger("transition", transitionValue);
+            anim.SetBool("attacking", false);
+            IsReady = false;
+        }
     }
 }
